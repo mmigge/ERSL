2541ab9dbc1c8943c5c4b127093f710fff03df2d
NiklasPhilippi <n_phil01@uni-muenster.de>
2019-11-12 16:13:08 +0100

    Added download code (dummy)

--- imageAquisition.py
+++ imageAquisition.py
@@ -1,13 +1,5 @@
-#from landsat.google_download import GoogleDownload
-#
-#x = GoogleDownload(start='1983-01-01', end='1983-06-28', satellite=4, latitude="52.29", longitude="18.4",
-#                   output_path='/home/niklas/Uni/02_03_thirdMaster/remoteSensing/images/lowClouds',
-#                   max_cloud_percent=20)
-##low = x.candidate_scenes(return_list=True)
-#print(low)
-#x.download()
-
 import landsatxplore.api
+from landsatxplore.earthexplorer import EarthExplorer
 
 # Initialize a new API instance and get an access key
 api = landsatxplore.api.API("remoteSensingErsl", "ErslRemoteSensing00")
@@ -19,7 +11,7 @@ scenes = api.search(
     start_date='1983-01-01',
     end_date='1995-12-31',
     max_cloud_cover=10,
-    max_results=2000)
+    max_results=20)
 
 print('{} scenes found.'.format(len(scenes)))
 
@@ -27,3 +19,10 @@ for scene in scenes:
     print(scene['acquisitionDate'])
 
 api.logout()
+
+# Start downloading
+ee = EarthExplorer("remoteSensingErsl", "ErslRemoteSensing00")
+
+ee.download(scene_id='LT51960471995178MPS00', output_dir='/home/niklas/Uni/')
+
+ee.logout()

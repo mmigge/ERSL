06c0d6c37fb4d5da645518dcf590b2a924c29446
nPhil01 <n_phil01@uni-muenster.de>
2019-12-17 11:47:23 +0100

    Increased timespan for searching images; More detailed Exception printing; Bugfix: Just extracting tar.gz files

--- imageAquisition.py
+++ imageAquisition.py
@@ -1,6 +1,7 @@
 import os
 from datetime import date, datetime
 import tarfile
+import traceback
 import landsatxplore.api
 from landsatxplore.earthexplorer import EarthExplorer
 
@@ -14,7 +15,7 @@ scenes_early = api.search(
     bbox=(51.1931, 11.2830, 53.5990, 14.8865),
     start_date='1981-01-01',
     end_date='1983-12-31',
-    max_cloud_cover=10,
+    max_cloud_cover=15,
     max_results=1000)
 
 # Images after 1984
@@ -23,23 +24,17 @@ scenes = api.search(
     bbox=(51.1931, 11.2830, 53.5990, 14.8865),
     start_date='1984-01-01',
     end_date='1995-12-31',
-    max_cloud_cover=10,
+    max_cloud_cover=15,
     max_results=1000)
 
 today = date.today()
-datemin = datetime.strptime("06-01", "%m-%d").date().replace(year=today.year)
-datemax = datetime.strptime("07-31", "%m-%d").date().replace(year=today.year)
+datemin = datetime.strptime("05-15", "%m-%d").date().replace(year=today.year)
+datemax = datetime.strptime("08-15", "%m-%d").date().replace(year=today.year)
 selected_scenes = []
 
-for scene in scenes_early:
-    time = scene['acquisitionDate']
-    if time[-5:] != "02-29":
-        scenedate = datetime.strptime(time, '%Y-%m-%d').date().replace(year=today.year)
+combined_scenes = scenes_early + scenes
 
-        if datemin < scenedate < datemax:
-            selected_scenes.append(scene)
-
-for scene in scenes:
+for scene in combined_scenes:
     time = scene['acquisitionDate']
     if time[-5:] != "02-29":
         scenedate = datetime.strptime(time, '%Y-%m-%d').date().replace(year=today.year)
@@ -69,9 +64,8 @@ for scene in selected_scenes:
     if not os.path.isfile(current_directory + '/images/' + year + '/' + display_id + '.tar.gz'):
         try:
             ee.download(scene_id=scene_id, output_dir=current_directory + '/images/' + year)
-        except Exception as err:
-            print("An Error has occurred:")
-            print(err)
+        except Exception:
+            traceback.print_exc()
     else:
         print("File " + display_id + " already exists")
 
@@ -81,7 +75,8 @@ ee.logout()
 images = current_directory + '/images/'
 for subdir, dirs, files in os.walk(images):
     for file in files:
-        with tarfile.open(subdir + '/' + file, 'r:gz') as tar:
-            substring_index = file.find('.')
-            extract_dir = subdir + '/' + file[:substring_index]
-            tar.extractall(extract_dir)
+        if file.endswith('tar.gz'):
+            with tarfile.open(subdir + '/' + file, 'r:gz') as tar:
+                substring_index = file.find('.')
+                extract_dir = subdir + '/' + file[:substring_index]
+                tar.extractall(extract_dir)

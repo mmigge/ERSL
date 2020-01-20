1a6895f8f7cb8e9edecddf5cf30e21a47daae4bd
Moritz Migge <moritz.migge@web.de>
2020-01-21 00:58:18 +0100

    Update for iMVrt.py: Path unifiers + comments

--- imageMergingVrt.py
+++ imageMergingVrt.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import traceback
 
-images = os.getcwd() + '/images/'
+images = os.path.join(os.getcwd(),'images')
 
 subdirs = os.listdir(images)
 
@@ -29,36 +29,39 @@ def minimum(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize,
 """
 
 for direc in subdirs:
-    path = images + direc + '/'
-    merge_command_1 = ['gdalbuildvrt', '-allow_projection_difference', images + direc + '/min_merged_B1.vrt', '-srcnodata', '0']
-    merge_command_2 = ['gdalbuildvrt', '-allow_projection_difference', images + direc + '/min_merged_B2.vrt', '-srcnodata', '0']
-    merge_command_3 = ['gdalbuildvrt', '-allow_projection_difference', images + direc + '/min_merged_B3.vrt', '-srcnodata', '0']
-    merge_command_4 = ['gdalbuildvrt', '-allow_projection_difference', images + direc + '/min_merged_B4.vrt', '-srcnodata', '0']
-    merge_command_5 = ['gdalbuildvrt', '-allow_projection_difference', images + direc + '/min_merged_B5.vrt', '-srcnodata', '0']
-    merge_command_6 = ['gdalbuildvrt', '-allow_projection_difference', images + direc + '/min_merged_B6.vrt', '-srcnodata', '0']
-    merge_command_7 = ['gdalbuildvrt', '-allow_projection_difference', images + direc + '/min_merged_B7.vrt', '-srcnodata', '0']
+    img_path = os.path.join(images, direc)
+    merge_command_1 = ['gdalbuildvrt', '-allow_projection_difference', os.path.join(img_path,'min_merged_B1.vrt'), '-srcnodata', '0']
+    merge_command_2 = ['gdalbuildvrt', '-allow_projection_difference', os.path.join(img_path,'min_merged_B2.vrt'), '-srcnodata', '0']
+    merge_command_3 = ['gdalbuildvrt', '-allow_projection_difference', os.path.join(img_path,'min_merged_B3.vrt'), '-srcnodata', '0']
+    merge_command_4 = ['gdalbuildvrt', '-allow_projection_difference', os.path.join(img_path,'min_merged_B4.vrt'), '-srcnodata', '0']
+    merge_command_5 = ['gdalbuildvrt', '-allow_projection_difference', os.path.join(img_path,'min_merged_B5.vrt'), '-srcnodata', '0']
+    merge_command_6 = ['gdalbuildvrt', '-allow_projection_difference', os.path.join(img_path,'min_merged_B6.vrt'), '-srcnodata', '0']
+    merge_command_7 = ['gdalbuildvrt', '-allow_projection_difference', os.path.join(img_path,'min_merged_B7.vrt'), '-srcnodata', '0']
+
+    # Wird nur einmal weiter unten genutzt (if command.__len__() >= min_command_length)
     min_command_length = merge_command_1.__len__() + 1
-    for subdir, dirs, files in os.walk(path):
+    for subdir, dirs, files in os.walk(img_path):
         if subdir.endswith('T1') or subdir.endswith('T2'):
             for file in os.listdir(subdir):
                 if file.endswith('B1.TIF'):
-                    merge_command_1.append(subdir + '/' + file)
+                    merge_command_1.append(os.path.join(subdir, file))
                 elif file.endswith('B2.TIF'):
-                    merge_command_2.append(subdir + '/' + file)
+                    merge_command_2.append(os.path.join(subdir, file))
                 elif file.endswith('B3.TIF'):
-                    merge_command_3.append(subdir + '/' + file)
+                    merge_command_3.append(os.path.join(subdir, file))
                 elif file.endswith('B4.TIF'):
-                    merge_command_4.append(subdir + '/' + file)
+                    merge_command_4.append(os.path.join(subdir, file))
                 elif file.endswith('B5.TIF'):
-                    merge_command_5.append(subdir + '/' + file)
+                    merge_command_5.append(os.path.join(subdir, file))
                 elif file.endswith('B6.TIF'):
-                    merge_command_6.append(subdir + '/' + file)
+                    merge_command_6.append(os.path.join(subdir, file))
                 elif file.endswith('B7.TIF'):
-                    merge_command_7.append(subdir + '/' + file)
+                    merge_command_7.append(os.path.join(subdir, file))
 
-    commands = [merge_command_1, merge_command_2, merge_command_3, merge_command_4, merge_command_5, merge_command_6,
-                merge_command_7]
+    commands = [merge_command_1, merge_command_2, merge_command_3, merge_command_4, merge_command_5, merge_command_6, merge_command_7]
     for command in commands:
+
+        # Diese Bedingung trifft immer zu, oder nicht? 
         if command.__len__() >= min_command_length:
             try:
                 ps = subprocess.Popen(
@@ -71,9 +74,9 @@ for direc in subdirs:
             except Exception:
                 traceback.print_exc()
 
-    for files in os.listdir(path):
+    for files in os.listdir(img_path):
         if files.endswith('vrt'):
-            f = open(path + files, "r")
+            f = open(img_path + files, "r")
             contents = f.readlines()
             f.close()
 
@@ -81,16 +84,16 @@ for direc in subdirs:
             contents[index - 1] = '<VRTRasterBand dataType="Byte" band="1" subClass="VRTDerivedRasterBand">'
             contents.insert(index, pixel_function)
 
-            f = open(path + files, "w")
+            f = open(img_path + files, "w")
             contents = "".join(contents)
             f.write(contents)
             f.close()
 
-            translate_command_1 = ['gdal_translate', '--config', 'GDAL_VRT_ENABLE_PYTHON', 'YES', path + files,
-                                   path + 'min_merged_B' + files[-5: -4] + '.tif']
+            translate_command = ['gdal_translate', '--config', 'GDAL_VRT_ENABLE_PYTHON', 'YES', img_path + files,
+                                   img_path + 'min_merged_B' + files[-5: -4] + '.tif']
 
             ps = subprocess.Popen(
-                translate_command_1,
+                translate_command,
                 stdout=subprocess.PIPE
             )
             output = ps.communicate()[0]
